a7bdc9bdd64d5a7e33f916744b32d102633cf74c
abhishek <abhishek@maitretech.com>
2023-09-19 14:05:54 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+import { uiActions } from './store/ui-slice';
+import { useSelector ,useDispatch } from 'react-redux';
+export default function Nav() {
+  const dispatch= useDispatch()
+    const handlerToggle =()=>{
+        dispatch(uiActions.toggle())
+    }
+    const count = useSelector((state) => state.ui.cartIsVisible);    
+    return (
+
+    <div className='w-full flex justify-around items-center h-10 bg-orange-500'>
+      <h1>React</h1>
+      <div className='border-solid border-2 border-indigo-600 p-1 ' onClick={handlerToggle}>
+        <span >cart</span><span className='' >0</span>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Section1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Section() {
+  return (
+    <div className='h-screen flex justify-center items-center w-full bg-green-400'>
+      <h1>comming soon</h1>
+    </div>
+  )
+}
